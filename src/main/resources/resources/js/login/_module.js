e04f9e8113346a93334f9371139db27fa1f5e660
mikael0 <mike-bmw@mail.ru>
2016-11-21 10:45:12 +0300

    Login page redesign

--- src/main/resources/resources/js/login/_module.js
+++ src/main/resources/resources/js/login/_module.js
@@ -8,4 +8,31 @@
  * @name LoginModule
  */
 
-var LoginModule = angular.module('Login', ['ngMaterial']);
+var LoginModule = angular.module('Login', ['ngMaterial'])
+.config(function($mdThemingProvider) {
+		$mdThemingProvider.definePalette('custom', {
+			'50': '2f2e2e',
+			'100': '2f2e2e',
+			'200': '2f2e2e',
+			'300': '2f2e2e',
+			'400': '2f2e2e',
+			'500': '2f2e2e',
+			'600': '2f2e2e',
+			'700': '2f2e2e',
+			'800': '2f2e2e',
+			'900': '2f2e2e',
+			'A100': '44dbbe',
+			'A200': '44dbbe',
+			'A400': '44dbbe',
+			'A700': '44dbbe',
+			'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+			// on this palette should be dark or light
+
+			'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
+			'200', '300', '400', 'A100'],
+			'contrastLightColors': undefined    // could also specify this if default was 'dark'
+		});
+		$mdThemingProvider.theme("default").primaryPalette("custom")
+						   .accentPalette("custom")
+						   .warnPalette("custom");
+});
